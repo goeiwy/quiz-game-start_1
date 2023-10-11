5bfb702729f6fde3598c226d6237b77d1a33cf59
goeiwy <146850262+goeiwy@users.noreply.github.com>
2023-10-11 18:37:36 +0800

    Add files via upload

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,11 @@
+class Question():
+    def __init__(self,question_text, answer):
+        self.text = question_text
+        self.answer = answer
+
+#TODO 1: asking the questions
+#TODO 2: check if answer was correct
+#TODO 3: check if we are at end of quiz.
+
+#a = hello
+#b = true
